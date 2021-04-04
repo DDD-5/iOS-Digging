4b2e7bdcb913f7ce1704df8f19e39f67e5971cd1
Jaewook <hjw0911@gmail.com>
2021-04-05 02:08:25 +0900

    Add MyDiggingView

---
+++ Digging/Digging/Views/MyDigging/MyDiggingView.swift
@@ -0,0 +1,22 @@
+//
+//  MyDiggingView.swift
+//  Digging
+//
+//  Created by 황재욱 on 2021/04/05.
+//
+
+import SwiftUI
+
+struct MyDiggingView: View {
+    var body: some View {
+      VStack {
+        Text("마이 디깅")
+      }
+    }
+}
+
+struct MyDiggingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MyDiggingView()
+    }
+}
