35f3d454283c53a527d2652ce45ad14af8eca2bd
Jaewook <hjw0911@gmail.com>
2021-04-05 01:51:44 +0900

    Add SearchDiggingView

---
+++ Digging/Digging/Views/SearchDigging/SearchDiggingView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchDiggingView.swift
+//  Digging
+//
+//  Created by 황재욱 on 2021/04/05.
+//
+
+import SwiftUI
+
+struct SearchDiggingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchDiggingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchDiggingView()
+    }
+}
