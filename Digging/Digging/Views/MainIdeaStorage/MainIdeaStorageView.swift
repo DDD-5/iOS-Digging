b7c858d7a6d8de45dfa7407cb96d59dbfe4c78f6
Jaewook <hjw0911@gmail.com>
2021-04-06 21:59:54 +0900

    Update folder list with ListView

--- Digging/Digging/Views/MainIdeaStorage/MainIdeaStorageView.swift
+++ Digging/Digging/Views/MainIdeaStorage/MainIdeaStorageView.swift
@@ -14,39 +14,42 @@ enum DiggingFolderType {
 }
 
 struct MainIdeaStorageView: View {
-    var body: some View {
-      ScrollView {
-        VStack {
-          HStack {
-            Text("Digging과 함께\n영감을 수집해요💥")
-            Spacer()
-            Image("digging_main_top")
-          }
-          .padding(
-            EdgeInsets(
-              top: 40,
-              leading: 20,
-              bottom: 30,
-              trailing: 20
-            )
+  var body: some View {
+    List {
+      HStack {
+        Text("Digging과 함께\n영감을 수집해요💥")
+          .font(
+            .custom("AppleSDGothicNeo-Bold", size: 24)
           )
-          Section(
-            header: Text("💡 내가 디깅한 영감"))
-          {
-            Text("Test")
-          }
-          Section(
-            header: Text("💡 최근에 디깅한 영감"))
-          {
-            Text("Test")
-          }
-        }
+        Spacer()
+        Image("digging_main_top")
+      }
+      .padding(
+        EdgeInsets(
+          top: 20,
+          leading: 20,
+          bottom: 32,
+          trailing: 20
+        )
+      )
+      Section(
+        header: Text("💡 내가 디깅한 영감")
+      )
+      {
+        Text("Test")
+      }
+      Section(
+        header: Text("💡 최근에 디깅한 영감"))
+      {
+        Text("Test")
       }
     }
+    .listRowInsets(.none)
+  }
 }
 
 struct MainIdeaStorageView_Previews: PreviewProvider {
-    static var previews: some View {
-      MainIdeaStorageView()
-    }
+  static var previews: some View {
+    MainIdeaStorageView()
+  }
 }
