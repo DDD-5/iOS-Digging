8eb1760343f04d4e203ee2b86074907fa5f16ba1
Jaewook <hjw0911@gmail.com>
2021-04-07 17:40:01 +0900

    Update MainIdeaStorageView with NavigationView

--- Digging/Digging/Views/MainIdeaStorage/MainIdeaStorageView.swift
+++ Digging/Digging/Views/MainIdeaStorage/MainIdeaStorageView.swift
@@ -7,44 +7,51 @@
 
 import SwiftUI
 
-enum DiggingFolderType {
-  case text
-  case image
-  case link
-}
-
 struct MainIdeaStorageView: View {
+  
+  @ObservedObject
+  var viewModel = MainIdeaStorageViewModel()
+  
   var body: some View {
-    List {
-      HStack {
-        Text("Digging과 함께\n영감을 수집해요💥")
-          .font(
-            .custom("AppleSDGothicNeo-Bold", size: 24)
+    NavigationView {
+      List {
+        HStack {
+          Text("Digging과 함께\n영감을 수집해요💥")
+            .font(
+              .custom("AppleSDGothicNeo-Bold", size: 24)
+            )
+          Spacer()
+          Image("digging_main_top")
+        }
+        .padding(
+          EdgeInsets(
+            top: 20,
+            leading: 20,
+            bottom: 32,
+            trailing: 20
           )
-        Spacer()
-        Image("digging_main_top")
-      }
-      .padding(
-        EdgeInsets(
-          top: 20,
-          leading: 20,
-          bottom: 32,
-          trailing: 20
         )
-      )
-      Section(
-        header: Text("💡 내가 디깅한 영감")
-      )
-      {
-        Text("Test")
+        Section(
+          header: Text("💡 내가 디깅한 영감")
+        )
+        {
+          ForEach(viewModel.folderInfoArray) { folderInfo in
+            NavigationLink(
+              destination: StoredDiggingListView())
+              {
+              DiggingFolderView(title: "", description: "")
+            }
+          }
+        }
+        Section(
+          header: Text("💡 최근에 디깅한 영감"))
+        {
+          Text("Test")
+        }
       }
-      Section(
-        header: Text("💡 최근에 디깅한 영감"))
-      {
-        Text("Test")
-      }
+      .navigationBarHidden(true)
+      .listRowInsets(.none)
     }
-    .listRowInsets(.none)
   }
 }
 
