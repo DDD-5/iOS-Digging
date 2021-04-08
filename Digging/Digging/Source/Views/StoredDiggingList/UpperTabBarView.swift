dcaade4bf7f9c00ea728fb07b360fa152bbcf37b
Jaewook <hjw0911@gmail.com>
2021-04-08 14:58:03 +0900

    Add UpperTabBarView base implementation

---
+++ Digging/Digging/Source/Views/StoredDiggingList/UpperTabBarView.swift
@@ -0,0 +1,58 @@
+//
+//  UpperTabBarView.swift
+//  Digging
+//
+//  Created by 황재욱 on 2021/04/08.
+//  Copyright © 2021 Oreo. All rights reserved.
+//
+
+import SwiftUI
+
+enum SelectedType {
+  case total
+  case liked
+}
+
+struct UpperTabBarView: View {
+  @Binding var selection: SelectedType
+  
+  var body: some View {
+    VStack {
+      HStack {
+        Spacer()
+        Button(action: {
+          
+        }, label: {
+          Text("전체")
+            .font(.custom("AppleSDGothicNeo-Bold", size: 16)
+            )
+            .foregroundColor(.black)
+        })
+        Spacer()
+        Spacer()
+        Button(action: {
+          
+        }, label: {
+          Text("좋아요")
+            .font(.custom("AppleSDGothicNeo-Bold", size: 16)
+            )
+            .foregroundColor(.black)
+        })
+        Spacer()
+      }
+      HStack {
+        Rectangle()
+          .frame(height: 3, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+          .padding()
+        Spacer()
+      }
+    }
+  }
+}
+
+struct UpperTabBarView_Previews: PreviewProvider {
+  @State static var selection: SelectedType = .total
+  static var previews: some View {
+    UpperTabBarView(selection: $selection)
+  }
+}
