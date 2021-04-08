5f273e754abf60e8eafbd5ef81aad2096909a64f
Jaewook <hjw0911@gmail.com>
2021-04-08 14:57:31 +0900

    Add MainIdeaStorageHeaderView

---
+++ Digging/Digging/Source/Views/MainIdeaStorage/MainIdeaStorageHeaderView.swift
@@ -0,0 +1,25 @@
+//
+//  MainIdeaStorageHeaderView.swift
+//  Digging
+//
+//  Created by 황재욱 on 2021/04/08.
+//  Copyright © 2021 Oreo. All rights reserved.
+//
+
+import SwiftUI
+
+struct MainIdeaStorageHeaderView: View {
+  var title: String
+  
+  var body: some View {
+    Text(title)
+      .frame(width: UIScreen.main.bounds.width, height: 48, alignment: .topLeading)
+      .background(Color.white)
+  }
+}
+
+struct MainIdeaStorageHeaderView_Previews: PreviewProvider {
+  static var previews: some View {
+    MainIdeaStorageHeaderView(title: "title")
+  }
+}
