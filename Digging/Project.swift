f4079bf8fe4c6de938ba56dd6e4f5c22c944ebfc
Gisu <kisu9838@gmail.com>
2021-04-20 17:54:10 +0900

    Update Project.swift
    - TargetAction 추가, cocoapods 추가

--- Digging/Project.swift
+++ Digging/Project.swift
@@ -11,6 +11,16 @@ let organizationName = "Oreo"
 let bundleID = "com.oreo.ddd.Digging"
 
 
+let targetAction = [
+	TargetAction.pre(path: "Scripts/RSwiftRunScript.sh",
+					 arguments: [],
+					 name: "R.Swift",
+					 inputPaths: [Path.init("$TEMP_DIR/rswift-lastrun")],
+					 inputFileListPaths: [],
+					 outputPaths: [Path.init("$SRCROOT/\(projectName)/Source/Generated/R.generated.swift")],
+					 outputFileListPaths: [])
+]
+
 // TODO: 엑스코드 프리뷰 이슈로 현재 선언만 한 상태입니다. 확인후 Project 생성자에 주입예정
 let settings = Settings(configurations: [
 	.debug(name: "Debug", xcconfig: "\(projectName)/SupportFiles/Config/Debug.xcconfig"),
@@ -55,6 +65,10 @@ let project = Project(
 						],
 						resources: [
 							"\(projectName)/SupportFiles/Resource/**"
+						],
+						actions: targetAction,
+						dependencies: [
+							.cocoapods(path: ".")
 						]
 					
 				)
