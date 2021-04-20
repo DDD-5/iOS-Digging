3be23ee5fa9a9d91db2350932728386568e9e832
Jaewook <hjw0911@gmail.com>
2021-04-20 20:35:57 +0900

    feat: Add SheetPresenter for test in Login

--- Digging/Digging/Source/Views/TabBar/MainTabBarView.swift
+++ Digging/Digging/Source/Views/TabBar/MainTabBarView.swift
@@ -7,7 +7,23 @@
 
 import SwiftUI
 
+struct SheetPresenter<Content>: View where Content: View {
+  @Binding var presentingSheet: Bool
+  var content: Content
+  var body: some View {
+    Text("")
+      .sheet(isPresented: self.$presentingSheet, content: { self.content })
+      .onAppear {
+        DispatchQueue.main.async {
+          self.presentingSheet = true
+        }
+      }
+  }
+}
+
 struct MainTabBarView: View {
+  @State var newListingPresented = false
+  
   var body: some View {
     NavigationView {
       TabView {
@@ -15,22 +31,29 @@ struct MainTabBarView: View {
           .tabItem {
             Image("tabbar_main_idea_storage")
             Text("보관함")
-          }
-        MyDiggingView()
+          }.tag(1)
+        
+        SheetPresenter(presentingSheet: $newListingPresented, content: MyDiggingView())
           .tabItem {
-          Image("tabbar_create_digging")
-          Text("디깅")
-        }
+            Image("tabbar_create_digging")
+            Text("디깅")
+          }
+          .tag(2)
+        /*MyDiggingView()
+         .tabItem {
+         Image("tabbar_create_digging")
+         Text("디깅")
+         }*/
         SearchDiggingView()
           .tabItem {
             Image("tabbar_search_digging")
             Text("검색")
-          }
+          }.tag(3)
         MyDiggingView()
           .tabItem {
             Image("tabbar_my_digging")
             Text("마이 디깅")
-          }
+          }.tag(4)
       }
       .accentColor(.black)
     }
