41f2a17be454bc8c42189be16dc015390072ca5f
Jaewook <hjw0911@gmail.com>
2021-04-23 00:14:56 +0900

    feat: Add Color extension with grayBorder color

---
+++ Digging/Digging/Source/Extension/Color+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  Color+Extension.swift
+//  Digging
+//
+//  Created by 황재욱 on 2021/04/22.
+//  Copyright © 2021 Oreo. All rights reserved.
+//
+
+import SwiftUI
+
+extension Color {
+  static let grayBorder = Color("grayBorder")
+}
