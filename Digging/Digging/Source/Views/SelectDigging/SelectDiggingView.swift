c230d46d7981405a5b7b2024a048530037c6a34e
Gisu <kisu9838@gmail.com>
2021-04-23 19:01:50 +0900

    Update List View

--- Digging/Digging/Source/Views/SelectDigging/SelectDiggingView.swift
+++ Digging/Digging/Source/Views/SelectDigging/SelectDiggingView.swift
@@ -26,63 +26,19 @@ struct SelectDiggingView: View {
 							EdgeInsets(top: 20, leading: 20, bottom: 20, trailing: 20)
 						)
 				}
-				Text("로그인 또는 회원 가입")
+				Text("템플릿 선택하기")
 					.frame(maxWidth: .infinity, alignment: .leading)
 					.font(Font.custom("AppleSDGothicNeo-Bold", size: 16))
 
 			}
 			Divider()
+			// TODO: 뷰모델로 처리예정
+			List{
+				SelectDiggingCategoryView(title: "텍스트", image: R.image.digging_text_folder_img.name)
+				SelectDiggingCategoryView(title: "사진", image: R.image.digging_text_folder_img.name)
+				SelectDiggingCategoryView(title: "링크", image: R.image.digging_text_folder_img.name)
 
-			VStack(spacing: 20, content: {
-				Text("디깅에서 나만의 영감을 디깅하고 싶다면,\n지금 간단하게 시작해보세요!")
-					.multilineTextAlignment(.center)
-					.font(Font.custom("AppleSDGothicNeo-Regular", size: 14))
-
-				Button(action:{print("구글 로그인")}){
-					HStack(alignment: .center) {
-						Spacer()
-						Image(R.image.close.name)
-							.resizable()
-							.frame(width: 24, height: 24)
-						Text("구글 로그인")
-							.font(Font.custom("AppleSDGothicNeo-Bold", size: 16))
-							.foregroundColor(Color(red: 96 / 255, green: 96 / 255, blue: 96 / 255)
-							)
-
-						Spacer()
-					}
-					.padding(EdgeInsets(top: 10, leading: 0, bottom: 10, trailing: 0))
-
-
-				}
-				.background(RoundedRectangle(cornerRadius: 8).stroke(Color.black, lineWidth: /*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/))
-				.padding(EdgeInsets(top: 0, leading: 38, bottom: 0, trailing: 38))
-
-				Button(action:{print("애플 로그인")}){
-					HStack {
-						Spacer()
-
-						Image(R.image.close.name)
-							.resizable()
-							.frame(width: 24, height: 24)
-						Text("애플 로그인")
-							.font(Font.custom("AppleSDGothicNeo-Bold", size: 16))
-							.foregroundColor(Color.white)
-
-						Spacer()
-					}
-					.padding(EdgeInsets(top: 10, leading: 0, bottom: 10, trailing: 0))
-
-				}
-				.background(Color.black)
-				.cornerRadius(8)
-				.padding(EdgeInsets(top: 0, leading: 38, bottom: 0, trailing: 38))
-
-
-
-			})
-			.padding(EdgeInsets(top: 10, leading: 10, bottom: 0, trailing: 10))
-
+			}
 		}
 		.background(Color.white)
 
