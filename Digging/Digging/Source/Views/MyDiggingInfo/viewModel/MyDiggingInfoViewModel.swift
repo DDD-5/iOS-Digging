8acd195de86cb02bba3a43cc47ef85294150358d
Jaewook <hjw0911@gmail.com>
2021-04-26 22:10:44 +0900

    feat: Add MyDiggingInfoViewModel

---
+++ Digging/Digging/Source/Views/MyDiggingInfo/viewModel/MyDiggingInfoViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  MyDiggingInfoViewModel.swift
+//  Digging
+//
+//  Created by 황재욱 on 2021/04/26.
+//  Copyright © 2021 Oreo. All rights reserved.
+//
+
+import Foundation
+
+class MyDiggingInfoViewModel {
+  
+}
