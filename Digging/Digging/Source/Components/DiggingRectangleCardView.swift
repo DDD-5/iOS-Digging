e1ac885137b48a4c842387595af94b2d10a717f4
Jaewook <hjw0911@gmail.com>
2021-04-29 19:56:09 +0900

    feat: Add DiggingRectangleCardView

---
+++ Digging/Digging/Source/Components/DiggingRectangleCardView.swift
@@ -0,0 +1,21 @@
+//
+//  DiggingRectangleCardView.swift
+//  Digging
+//
+//  Created by 황재욱 on 2021/04/29.
+//  Copyright © 2021 Oreo. All rights reserved.
+//
+
+import SwiftUI
+
+struct DiggingRectangleCardView: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+struct DiggingRectangleCardView_Previews: PreviewProvider {
+  static var previews: some View {
+    DiggingRectangleCardView()
+  }
+}
