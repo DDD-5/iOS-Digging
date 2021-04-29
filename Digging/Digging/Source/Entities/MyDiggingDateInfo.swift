7d47962fdead7affdb2510acb71df2a5d591468d
Jaewook <hjw0911@gmail.com>
2021-04-29 19:56:30 +0900

    feat: Add temp entity MyDiggingDateInfo

---
+++ Digging/Digging/Source/Entities/MyDiggingDateInfo.swift
@@ -0,0 +1,15 @@
+//
+//  MyDiggingDateInfo.swift
+//  Digging
+//
+//  Created by 황재욱 on 2021/04/29.
+//  Copyright © 2021 Oreo. All rights reserved.
+//
+
+import Foundation
+
+struct MyDiggingDateInfo {
+  let id: Int
+  let dataNumber: Int
+  let isStored: Bool
+}
