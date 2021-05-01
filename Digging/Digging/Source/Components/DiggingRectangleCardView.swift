9f092f16521ed95303bbb22d1c4d1934b0ced342
Jaewook <hjw0911@gmail.com>
2021-05-01 16:38:27 +0900

    feat: Update DiggingRectangleCardView UI

--- Digging/Digging/Source/Components/DiggingRectangleCardView.swift
+++ Digging/Digging/Source/Components/DiggingRectangleCardView.swift
@@ -10,7 +10,39 @@ import SwiftUI
 
 struct DiggingRectangleCardView: View {
   var body: some View {
-    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    VStack(alignment: .leading) {
+      HStack(alignment: .top) {
+        Image("digging_text_folder_small_img")
+          .frame(width: 20, height: 20)
+        Text("제목디자인제목텍스트").modifier(
+          DiggingFont(type: .bold, size: 14)
+        )
+      }
+      .padding([.top, .leading], 20)
+      .padding([.bottom], 12)
+      VStack(
+        alignment: .leading,
+        spacing: 8
+      ) {
+        Text("요즘 디깅 한다는 말을 많이 사용한다. 나도 인스타그램에 디깅을 많이 보긴 하는데 실제로 요즘 디깅 한다는 말을 많이 사용한다. 나 ···  ")
+          .modifier(
+            DiggingFont(type: .regular, size: 12)
+          )
+        Text("태그 | 디자인 | ui | 일반개발  ")
+          .foregroundColor(.lightGray)
+          .modifier(
+            DiggingFont(type: .regular, size: 12)
+          )
+      }
+      .padding( [.leading, .trailing], 20)
+      .padding(.bottom, 16)
+    }
+    .overlay(RoundedRectangle(cornerRadius: 8)
+              .stroke(
+                Color.grayBorder,
+                lineWidth: 1
+              )
+    )
   }
 }
 
