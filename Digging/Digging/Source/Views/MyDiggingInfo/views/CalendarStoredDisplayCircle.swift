7f1d3b7f86eea1cb8cad58c4e7d3fb8e6c6d2417
Jaewook <hjw0911@gmail.com>
2021-05-01 16:35:27 +0900

    feat: Add CalendarStoredDisplayCircle

---
+++ Digging/Digging/Source/Views/MyDiggingInfo/views/CalendarStoredDisplayCircle.swift
@@ -0,0 +1,25 @@
+//
+//  CalendarStoredDisplayCircle.swift
+//  Digging
+//
+//  Created by 황재욱 on 2021/04/29.
+//  Copyright © 2021 Oreo. All rights reserved.
+//
+
+import SwiftUI
+
+struct CalendarStoredDisplayCircle: View {
+  var body: some View {
+    Circle()
+      .frame(
+        width: 5,
+        height: 5
+      )
+  }
+}
+
+struct CalendarStoredDisplayCircle_Previews: PreviewProvider {
+  static var previews: some View {
+    CalendarStoredDisplayCircle()
+  }
+}
