9c121c1c936237d144bbe817fccbc8a3672aefc4
Jaewook <hjw0911@gmail.com>
2021-05-06 21:11:35 +0900

    feat: Update SearchDiggingView layout

--- Digging/Digging/Source/Presentation/Scenes/SearchDigging/SearchDiggingView.swift
+++ Digging/Digging/Source/Presentation/Scenes/SearchDigging/SearchDiggingView.swift
@@ -8,8 +8,11 @@
 import SwiftUI
 
 struct SearchDiggingView: View {
+  @State var searchedText: String = ""
+  @State var recommendedTagList = ["일반 개발", "웹 개발", "Javascript", "React", "Vue.js", "Angular", "Node.js"]
+  
   var body: some View {
-    VStack {
+    VStack(alignment: .leading) {
       Text("검색")
         .modifier(
           DiggingFont(
@@ -17,7 +20,39 @@ struct SearchDiggingView: View {
             size: 28
           )
         )
-    }
+      
+      TextField(LocalizedStringKey("  내가 디깅한 아이디어 찾아보기"), text: $searchedText)
+        .frame(height: 44)
+        .background(Color.textFieldGray.cornerRadius(8))
+      
+      Text("💡 내가 보유한 태그")
+        .modifier(
+          DiggingFont(
+            type: .bold,
+            size: 16
+          )
+        )
+        .padding()
+      
+      FlowLayout(mode: .scrollable,
+                                 binding: .constant(5),
+                                 items: $recommendedTagList) {
+        Text($0)
+          .modifier(DiggingFont(type: .medium, size: 14))
+          .padding([.leading, .trailing], 14)
+          .padding([.top, .bottom], 10)
+          .overlay(
+            RoundedRectangle(cornerRadius: 18)
+              .stroke(
+                Color.tagBorder,
+                lineWidth: 1
+              )
+          )
+      }
+      Spacer()
+      Spacer()
+    }.frame(height: UIScreen.main.bounds.height)
+    .padding([.leading, .trailing], 20)
   }
 }
 
