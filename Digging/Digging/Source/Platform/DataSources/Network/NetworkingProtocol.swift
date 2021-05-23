e58884ded51dcac7d738829449bf99d012a2a668
Gisu Hwang <kisu9838@gmail.com>
2021-05-23 22:32:01 +0900

    feat: Add NetworkingProtocol initialize

--- Digging/Digging/Source/Platform/DataSources/Network/NetworkingProtocol.swift
+++ Digging/Digging/Source/Platform/DataSources/Network/NetworkingProtocol.swift
@@ -19,3 +19,31 @@ extension NetworkingProtocol {
 		return self.request(target, file: file, function: function, line: line)
 	}
 }
+
+final class Networking: MoyaProvider<MultiTarget>, NetworkingProtocol {
+
+	
+	init(logger: [PluginType]) {
+		
+		let session = MoyaProvider<MultiTarget>.defaultAlamofireSession()
+		session.sessionConfiguration.timeoutIntervalForRequest = 10
+		super.init(requestClosure: { endpoint, completion in
+			do {
+				var urlRequest = try endpoint.urlRequest()
+				urlRequest.cachePolicy = .reloadIgnoringLocalAndRemoteCacheData
+			} catch MoyaError.requestMapping(let url) {
+				completion(.failure(MoyaError.requestMapping(url)))
+			} catch MoyaError.parameterEncoding(let error) {
+				completion(.failure(MoyaError.parameterEncoding(error)))
+			} catch {
+				completion(.failure(MoyaError.underlying(error, nil)))
+			}
+		}, session: session, plugins: logger)
+	}
+	
+	// TODO: Request 함수 구현 에정
+//	func request(_ target: TargetType, file: StaticString, function: StaticString, line: UInt) -> AnyPublisher<Response, MoyaError> {
+//		return self.requestPublisher(.target(target))
+//			.filterSuccessfulStatusCodes()
+//	}
+}
