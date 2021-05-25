27d079ecf7f64ef8b6319a6d842f696a4c4a3418
Gisu <kisu9838@gmail.com>
2021-05-25 14:04:57 +0900

    feat: Add DiggingRepository function

--- Digging/Digging/Source/Platform/Repositories/DiggingRepository.swift
+++ Digging/Digging/Source/Platform/Repositories/DiggingRepository.swift
@@ -8,19 +8,38 @@
 
 import Foundation
 import Combine
-
+import Moya
 protocol DiggingRepository: AnyObject {
-//	func createDigging() -> AnyPublisher<Data, Never>
-//	func diggingList(userID: String) -> AnyPublisher<Data, Never>
-//	func diggingDetail(diggingID: String) -> AnyPublisher<Data, Never>
-//	func totalTags(userID: String) -> AnyPublisher<Data, Never>
+	func createDigging<D>(type: D.Type) -> AnyPublisher<D, MoyaError>  where D : Decodable
+	func diggingList<D>(type: D.Type, userID: String) -> AnyPublisher<D, MoyaError>  where D : Decodable
+	func diggingDetail<D>(type: D.Type, diggingID: String) -> AnyPublisher<D, MoyaError>  where D : Decodable
+	func totalTags<D>(type: D.Type, userID: String) -> AnyPublisher<D, MoyaError>  where D : Decodable
 }
 
 class DefaultDiggingRepository: DiggingRepository {
 	private let networking: NetworkingProtocol
-	
+
 	init(networking: NetworkingProtocol) {
 		self.networking = networking
 	}
+
+	func createDigging<D>(type: D.Type) -> AnyPublisher<D, MoyaError> where D : Decodable {
+		return self.networking.request(DiggingServcie.createDigging)
+			.map(D.self)
+	}
+	func diggingList<D>(type: D.Type, userID: String) -> AnyPublisher<D, MoyaError> where D : Decodable {
+		return self.networking.request(DiggingServcie.diggingList(userID: userID))
+			.map(D.self)
+	}
+
+	func diggingDetail<D>(type: D.Type, diggingID: String) -> AnyPublisher<D, MoyaError> where D : Decodable {
+		return self.networking.request(DiggingServcie.diggingDetail(diggingID: diggingID))
+			.map(D.self)
+	}
 	
+	func totalTags<D>(type: D.Type, userID: String) -> AnyPublisher<D, MoyaError> where D : Decodable {
+		return self.networking.request(DiggingServcie.totalTags(userID: userID))
+			.map(D.self)
+	}
+
 }
