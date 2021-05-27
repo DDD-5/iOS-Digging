622ea631fa538ae801f1a75a5b2fd531cfc55c78
Gisu Hwang <kisu9838@gmail.com>
2021-05-27 21:43:29 +0900

    chore: Navigation inline

--- Digging/Digging/Source/Presentation/Scenes/DiggingDetail/DiggingImageDetailView.swift
+++ Digging/Digging/Source/Presentation/Scenes/DiggingDetail/DiggingImageDetailView.swift
@@ -15,96 +15,98 @@ struct DiggingImageDetailView: View {
 	var gridLayout = [
 		GridItem(.adaptive(minimum: 105, maximum: 105), spacing: 10)
 	]
-
-		var body: some View {
-			ScrollView {
-				VStack {
-					
-					DiggingDetailTitleView(title: "영감을 디깅하는 방법", date: "2021.04.21")
-					
-			
-					LazyVGrid(
-						columns: gridLayout,
-						alignment: .leading,
-						spacing: 15,
-						content: {
-							NavigationLink(
-								destination: DiggingTextDetailView(),
-								label: {
-									DiggingGridCellView {
-										
-									}.frame(height: 105)
-								})
+	
+	var body: some View {
+		ScrollView {
+			VStack {
+				
+				DiggingDetailTitleView(title: "영감을 디깅하는 방법", date: "2021.04.21")
+					.padding([.top], 20)
+				
+				
+				LazyVGrid(
+					columns: gridLayout,
+					alignment: .leading,
+					spacing: 15,
+					content: {
+						NavigationLink(
+							destination: DiggingTextDetailView(),
+							label: {
+								DiggingGridCellView {
+									
+								}.frame(height: 105)
+							})
 					})
 					.padding([.leading, .trailing], 20)
-					
-					Divider()
-						.padding([.leading, .trailing], 20)
-					Spacer()
-					
-					FlowLayout(mode: .scrollable,
-							   binding: .constant(5),
-							   items: $recommendedTagList) {
-						Text($0)
-							.foregroundColor(.white)
-							.modifier(DiggingFont(type: .medium, size: 14))
-							.padding([.leading, .trailing], 14)
-							.padding([.top, .bottom], 10)
-							.background(Color.tagFillColor.cornerRadius(18))
-					}
-					.padding([.top], 24)
+				
+				Divider()
 					.padding([.leading, .trailing], 20)
+				Spacer()
+				
+				FlowLayout(mode: .scrollable,
+									 binding: .constant(5),
+									 items: $recommendedTagList) {
+					Text($0)
+						.foregroundColor(.white)
+						.modifier(DiggingFont(type: .medium, size: 14))
+						.padding([.leading, .trailing], 14)
+						.padding([.top, .bottom], 10)
+						.background(Color.tagFillColor.cornerRadius(18))
 				}
-				.navigationBarItems(
-					leading:
-						HStack {
-							Button(action: {
-								self.presentationMode.wrappedValue.dismiss()
-							}) {
-								Image(R.image.back_btn_img.name)
-							}
-							DiggingFolderType.image.folderImage
+				.padding([.top], 24)
+				.padding([.leading, .trailing], 20)
+			}
+			.navigationBarItems(
+				leading:
+					HStack {
+						Button(action: {
+							self.presentationMode.wrappedValue.dismiss()
+						}) {
+							Image(R.image.back_btn_img.name)
+						}
+						DiggingFolderType.image.folderImage
+							.resizable()
+							.frame(width: 24, height: 24, alignment: .center)
+						
+						Text("이미지")
+						
+					},
+				trailing:
+					HStack {
+						Button(action: {
+							print("좋아요")
+						}) {
+							
+							Image(R.image.digging_like_deselect.name)
 								.resizable()
 								.frame(width: 24, height: 24, alignment: .center)
-							
-							Text("이미지")
-							
-						},
-					trailing:
-						HStack {
-							Button(action: {
-								print("좋아요")
-							}) {
-
-								Image(R.image.digging_like_deselect.name)
-									.resizable()
-									.frame(width: 24, height: 24, alignment: .center)
-							}
-							Button(action: {
-								print("작성")
-							}) {
-								Image(R.image.digging_write.name)
-									.resizable()
-									.frame(width: 24, height: 24, alignment: .center)
-							}
-							
-							Button(action: {
-								print("삭제")
-							}) {
-								Image(R.image.digging_delete.name)
-									.resizable()
-									.frame(width: 24, height: 24, alignment: .center)
-							}
-							
-						})
-				.navigationBarColor(backgroundColor: .white, tintColor: .clear)
-						.navigationBarBackButtonHidden(true)
-			}
+						}
+						Button(action: {
+							print("작성")
+						}) {
+							Image(R.image.digging_write.name)
+								.resizable()
+								.frame(width: 24, height: 24, alignment: .center)
+						}
+						
+						Button(action: {
+							print("삭제")
+						}) {
+							Image(R.image.digging_delete.name)
+								.resizable()
+								.frame(width: 24, height: 24, alignment: .center)
+						}
+						
+					})
+			.navigationBarTitleDisplayMode(.inline)
+			.navigationBarColor(backgroundColor: .white, tintColor: .clear)
+			.navigationBarBackButtonHidden(true)
 		}
+	}
 }
 
 struct DiggingImageDetailView_Previews: PreviewProvider {
-    static var previews: some View {
-        DiggingImageDetailView()
-    }
+	static var previews: some View {
+		DiggingImageDetailView()
+	}
 }
--- Digging/Digging/Source/Presentation/Scenes/DiggingDetail/DiggingTextDetailView.swift
+++ Digging/Digging/Source/Presentation/Scenes/DiggingDetail/DiggingTextDetailView.swift
@@ -16,20 +16,21 @@ struct DiggingTextDetailView: View {
 	var body: some View {
 		ScrollView {
 			VStack {
-
+				
 				DiggingDetailTitleView(title: "영감을 디깅하는 방법", date: "2021.04.21")
-
+					.padding([.top], 20)
+				
 				DiggingDetailContentTextView(contentText: """
 			요즘 디깅 한다는 말을 많이 사용한다. 나도 인스타그램에 디깅을 많이 보긴 하는데 실제로 사용해본 적은 없는것 같다. 요즘 디깅 한다는 말을 많이 사용한다. 나도 인스타그램에서 디깅을 많이 보긴 하는데 실제로 사용해본 적은 없는것 같다.요즘 디깅 한다는 말을 많이 사용한다. 나도 인스타그램에서 디깅을 많이 보긴 하는데 실제로 사용해본 적은 없는것 같다. 요즘 디
 			""")
-
+				
 				Divider()
 					.padding([.leading, .trailing], 20)
 				Spacer()
-
+				
 				FlowLayout(mode: .scrollable,
-						   binding: .constant(5),
-						   items: $recommendedTagList) {
+									 binding: .constant(5),
+									 items: $recommendedTagList) {
 					Text($0)
 						.foregroundColor(.white)
 						.modifier(DiggingFont(type: .medium, size: 14))
@@ -51,16 +52,16 @@ struct DiggingTextDetailView: View {
 						DiggingFolderType.text.folderImage
 							.resizable()
 							.frame(width: 24, height: 24, alignment: .center)
-
+						
 						Text("텍스트")
-
+						
 					},
 				trailing:
 					HStack {
 						Button(action: {
 							print("좋아요")
 						}) {
-
+							
 							Image(R.image.digging_like_deselect.name)
 								.resizable()
 								.frame(width: 24, height: 24, alignment: .center)
@@ -72,7 +73,7 @@ struct DiggingTextDetailView: View {
 								.resizable()
 								.frame(width: 24, height: 24, alignment: .center)
 						}
-
+						
 						Button(action: {
 							print("삭제")
 						}) {
@@ -80,16 +81,13 @@ struct DiggingTextDetailView: View {
 								.resizable()
 								.frame(width: 24, height: 24, alignment: .center)
 						}
-
+						
 					})
+			.navigationBarTitleDisplayMode(.inline)
 			.navigationBarColor(backgroundColor: .white, tintColor: .clear)
 			.navigationBarBackButtonHidden(true)
-
 		}
-
 	}
-
-	
 }
 
 struct DiggingTextDetailView_Previews: PreviewProvider {
