906ab775eaa44a27f2a8a9dd7fb83e8b5d332d08
Jaewook <hjw0911@gmail.com>
2021-05-27 20:00:02 +0900

    feat: Add GeneralDiggingInfo

---
+++ Digging/Digging/Source/Domain/Entities/GeneralDiggingInfo.swift
@@ -0,0 +1,13 @@
+//
+//  GeneralDiggingInfo.swift
+//  Digging
+//
+//  Created by 황재욱 on 2021/05/27.
+//  Copyright © 2021 Oreo. All rights reserved.
+//
+
+import Foundation
+
+struct GeneralDiggingInfo: Decodable {
+  let content: String?
+}
