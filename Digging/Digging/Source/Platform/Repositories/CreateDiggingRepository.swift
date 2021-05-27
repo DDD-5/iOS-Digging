5a336fd53043b43054a98aa4641bf84fb6627f1e
Jaewook <hjw0911@gmail.com>
2021-05-27 20:01:45 +0900

    feat: Add CreateDigginngRepository

---
+++ Digging/Digging/Source/Platform/Repositories/CreateDiggingRepository.swift
@@ -0,0 +1,33 @@
+//
+//  CreateDiggingRepository.swift
+//  Digging
+//
+//  Created by 황재욱 on 2021/05/27.
+//  Copyright © 2021 Oreo. All rights reserved.
+//
+
+import Combine
+import Foundation
+import Moya
+
+protocol CreateDiggingRepository {
+  func createTextDigging(textDiggingInfo: CreateTextDiggingInfo) -> AnyPublisher<GeneralDiggingInfo, Error>
+}
+
+class CreateDiggingRepositoryImpl: CreateDiggingRepository {
+  
+  private let networking: NetworkingProtocol
+
+  init(networking: NetworkingProtocol) {
+    self.networking = networking
+  }
+  
+  func createTextDigging(textDiggingInfo: CreateTextDiggingInfo) -> AnyPublisher<GeneralDiggingInfo, Error> {
+    
+    networking.request(CreateDiggingService.createTextDigging(requiredInfo: textDiggingInfo))
+      .map(\.data)
+      .decode(type: GeneralDiggingInfo.self, decoder: JSONDecoder()
+      )
+      .eraseToAnyPublisher()
+  }
+}
