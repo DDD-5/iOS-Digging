03df1fb609ca9af0a2325dac1ee73d068d66158b
Jaewook <hjw0911@gmail.com>
2021-05-28 20:11:31 +0900

    feat: Add CurrentDiggingWidgetView

---
+++ Digging/Digging/Source/Presentation/Components/CurrentDiggingWidgetView.swift
@@ -0,0 +1,21 @@
+//
+//  CurrentDiggingWidgetView.swift
+//  Digging
+//
+//  Created by 황재욱 on 2021/05/28.
+//  Copyright © 2021 Oreo. All rights reserved.
+//
+
+import SwiftUI
+
+struct CurrentDiggingWidgetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CurrentDiggingWidgetView_Previews: PreviewProvider {
+    static var previews: some View {
+        CurrentDiggingWidgetView()
+    }
+}
