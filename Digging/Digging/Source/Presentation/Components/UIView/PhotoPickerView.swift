4832c2c8704ee3d21099c58466e378e8905d29d6
Jaewook <hjw0911@gmail.com>
2021-06-01 22:53:08 +0900

    feat: Add PhotoPickerView

---
+++ Digging/Digging/Source/Presentation/Components/UIView/PhotoPickerView.swift
@@ -0,0 +1,27 @@
+//
+//  PhotoPickerView.swift
+//  Digging
+//
+//  Created by 황재욱 on 2021/06/01.
+//  Copyright © 2021 Oreo. All rights reserved.
+//
+
+import Photos
+import UIKit
+import SwiftUI
+
+struct PhotoPickerView: UIViewRepresentable {
+  
+  // MARK: - Properties
+  
+  // MARK: - Internal methods
+  
+  func makeUIView(context: Context) -> UIView {
+    let view = UIView()
+    return view
+  }
+  
+  func updateUIView(_ uiView: UIView, context: Context) {
+    
+  }
+}
