edae247b00049eef69386aa92d30200fb790b0d8
Jaewook <hjw0911@gmail.com>
2021-06-19 23:52:14 +0900

    feat: Update AddPhotoButtonView

--- Digging/Digging/Source/Presentation/Scenes/CreateDigging/Views/AddPhotoButtonView.swift
+++ Digging/Digging/Source/Presentation/Scenes/CreateDigging/Views/AddPhotoButtonView.swift
@@ -9,15 +9,36 @@
 import SwiftUI
 
 struct AddPhotoButtonView: View {
-    var body: some View {
-      Button(action: {}, label: {
-        Text("+")
-      })
-    }
+  
+  let buttonAction: () -> Void
+  
+  var body: some View {
+    Button(action: {
+      buttonAction()
+    }, label: {
+      Text("+")
+        .foregroundColor(.grayBorder)
+        .modifier(
+          DiggingFont(
+            type: .regular,
+            size: 30
+          )
+        )
+    })
+    .frame(
+      width: 105,
+      height: 105
+    )
+    .overlay(
+      RoundedRectangle(cornerRadius: 8)
+        .stroke(Color.grayBorder, lineWidth: 1)
+    )
+  }
 }
 
 struct AddPhotoButtonView_Previews: PreviewProvider {
-    static var previews: some View {
-        AddPhotoButtonView()
-    }
+  static var previews: some View {
+    AddPhotoButtonView(
+      buttonAction: { })
+  }
 }
