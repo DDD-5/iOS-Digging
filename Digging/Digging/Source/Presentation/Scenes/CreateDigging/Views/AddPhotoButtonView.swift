258c67184bf46563d3206d2e128bc658bb3b0430
Jaewook <hjw0911@gmail.com>
2021-06-19 22:48:23 +0900

    feat: Add AddPhotoButtonView

---
+++ Digging/Digging/Source/Presentation/Scenes/CreateDigging/Views/AddPhotoButtonView.swift
@@ -0,0 +1,23 @@
+//
+//  AddPhotoButtonView.swift
+//  Digging
+//
+//  Created by 황재욱 on 2021/06/19.
+//  Copyright © 2021 Oreo. All rights reserved.
+//
+
+import SwiftUI
+
+struct AddPhotoButtonView: View {
+    var body: some View {
+      Button(action: {}, label: {
+        Text("+")
+      })
+    }
+}
+
+struct AddPhotoButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddPhotoButtonView()
+    }
+}
