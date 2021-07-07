0f328cb9d73277f06a3b09f8429fbdfb5963dd10
Jaewook <hjw0911@gmail.com>
2021-07-07 22:18:01 +0900

    feat: Add MyDiggingInfoUseCase

---
+++ Digging/Digging/Source/Domain/UseCases/MyDiggingInfoUseCase.swift
@@ -0,0 +1,23 @@
+//
+//  MyDiggingInfoUseCase.swift
+//  Digging
+//
+//  Created by 황재욱 on 2021/07/07.
+//  Copyright © 2021 Oreo. All rights reserved.
+//
+
+import Combine
+
+class MyDiggingInfoUseCase {
+  let repository: MyDiggingInfoRepository
+  
+  init(repository: MyDiggingInfoRepository) {
+    self.repository = repository
+  }
+}
+
+extension MyDiggingInfoUseCase {
+  func requestMonthlyMyDiggingInfo(userID: Int, dateString: String) -> AnyPublisher<[MyDiggingDateInfo], Error> {
+    return repository.requetMonthlyDiggingInfo(userID: userID, dateString: dateString)
+  }
+}
