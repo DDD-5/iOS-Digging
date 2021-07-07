a4a52b6921a51bb524f7a0581d2fa89aa4dc0c90
Jaewook <hjw0911@gmail.com>
2021-07-07 22:17:40 +0900

    feat: Add MyDiggingInfoRepository

---
+++ Digging/Digging/Source/Platform/Repositories/MyDiggingInfoRepository.swift
@@ -0,0 +1,34 @@
+//
+//  MyDiggingInfoRepository.swift
+//  Digging
+//
+//  Created by 황재욱 on 2021/07/04.
+//  Copyright © 2021 Oreo. All rights reserved.
+//
+
+import Foundation
+import Moya
+import Combine
+
+protocol MyDiggingInfoRepository {
+  func requetMonthlyDiggingInfo(userID: Int, dateString: String) -> AnyPublisher<[MyDiggingDateInfo], Error>
+}
+
+class MyDiggingInfoRepositoryImpl: MyDiggingInfoRepository {
+  
+  let provider = MoyaProvider<MyDiggingInfoService>()
+  
+  func requetMonthlyDiggingInfo(userID: Int, dateString: String) -> AnyPublisher<[MyDiggingDateInfo], Error> {
+    return provider
+      .requestPublisher(.fetchMyDiggingInfoByMonth(userID: 0, dateString: ""))
+      .handleEvents(receiveOutput: { response in
+        print("response: \(response)")
+      })
+      .map(\.data)
+      .decode(
+        type: [MyDiggingDateInfo].self,
+        decoder: JSONDecoder()
+      )
+      .eraseToAnyPublisher()
+  }
+}
