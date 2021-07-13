95e9bd55f6ea959dc53d3b27ac0c1269caf899b3
Jaewook <hjw0911@gmail.com>
2021-07-13 21:37:58 +0900

    feat: ObservedObject 수정 ( 상속을 이용해서 하는 방법이 안됨;; )

--- Digging/Digging/Source/Presentation/Scenes/MyDiggingInfo/viewModel/MyDiggingInfoViewModel.swift
+++ Digging/Digging/Source/Presentation/Scenes/MyDiggingInfo/viewModel/MyDiggingInfoViewModel.swift
@@ -8,13 +8,19 @@
 
 import Foundation
 
-class MyDiggingInfoViewModel: BaseViewModel {
+import Combine
+
+class MyDiggingInfoViewModel: ObservableObject {
   
   let useCase: MyDiggingInfoUseCase
   
+  var subscriptions: Set<AnyCancellable> = .init()
+  
   let dayNameAlphabetArray = ["S", "M", "T", "W", "T", "F", "S"]
   
-  @Published var dateInfoArray: [MyDiggingDateInfo] = [MyDiggingDateInfo(date: "2021-06-21", day: "1", id: 1, isImage: nil, isLink: nil, isText: nil, resultCode: nil)]
+  @Published var firstDayInfo: MyDiggingDateInfo?
+  
+  @Published var dateInfoArray: [MyDiggingDateInfo] = []
   
   init(useCase: MyDiggingInfoUseCase) {
     self.useCase = useCase
@@ -26,12 +32,11 @@ extension MyDiggingInfoViewModel {
   func requestMonthlyMyDiggingInfo() {
     useCase.requestMonthlyMyDiggingInfo(userID: 1, dateString: "202106")
     .sink { _ in
-      print("completed")
     } receiveValue: { dateInfo in
-      DispatchQueue.main.async {
-        self.dateInfoArray.append(contentsOf: dateInfo)
+      DispatchQueue.main.async { [weak self] in
+        self?.dateInfoArray.append(contentsOf: dateInfo)
+        self?.firstDayInfo = dateInfo.first
       }
-      print("date info \(dateInfo)")
     }
     .store(in: &subscriptions)
   }
